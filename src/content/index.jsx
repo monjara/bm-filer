bf9138acd2ae48fb337ced72763a050a480ce370
monjara <yaginumee@gmail.com>
2024-05-25 14:39:42 +0900

    add path alias setting

--- src/content/index.jsx
+++ src/content/index.jsx
@@ -1,7 +1,7 @@
+import App from '@/App.jsx'
+import '@/content/index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '../App.jsx'
-import './index.css'
 
 const root = document.createElement('div')
 root.id = 'crx-root'
